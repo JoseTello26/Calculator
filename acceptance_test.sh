7f928c2dab53117375d2ec45b9bf1290ddfd4c89
JoseTello26 <39140021+JoseTello26@users.noreply.github.com>
2023-01-06 03:33:54 -0500

    err fix

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -2,4 +2,5 @@
 set -x
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
 NODE_PORT=$(kubectl get svc calculador-service -o=jsonpath='{.spec.ports[0].nodePort}')
-./calculador/mvnw acceptanceTest -Dcalculador.url=http://${NODE_IP}:${NODE_PORT}
+cd calculator
+./mvnw acceptanceTest -Dcalculador.url=http://${NODE_IP}:${NODE_PORT}
